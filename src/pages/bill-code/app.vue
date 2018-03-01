437276082ec0ac64409ca538b2c9af0fae6ae9c5
hujiajia <hujiajia@cpe-172-100-108-21.twcny.res.rr.com>
2018-03-01 13:01:02 +0800

    二维码位置调整

--- src/pages/bill-code/app.vue
+++ src/pages/bill-code/app.vue
@@ -9,7 +9,7 @@
 <script>
 import bridge from 'methods/bridge-v2'
 import config from 'methods/config'
-import QRCode from 'qrcode'
+import QRCode from 'qrcodejs2'
 
 export default {
   beforeRouteEnter (to, from, next) {
@@ -19,7 +19,6 @@ export default {
   },
   data () {
     return {
-      userid: ''
     }
   },
   created () {
@@ -48,17 +47,24 @@ export default {
       }).then((res) => {
         let data = res.data
         if (data.respcd === '0000') {
-          this.userid = data.data.hashid
-          this.urlToQrcode()
+          // this.urlToQrcode()
+          this.qrcode(data.data.hashid) // 简单方式生成二维码
         } else {
           this.$toast(res.resperr)
         }
 
       })
     },
+    qrcode (userid) {
+      let _qrcodeUrl = `${config.mHost}paydone/billcode-page.html?userid=` + userid
+      let qrcode = new QRCode('qrcodeContainer', {
+        width: window.innerWidth / 2.55,
+        height: window.innerWidth / 2.55, // 高度
+        text: _qrcodeUrl // 二维码内容
+      })
+    },
     urlToQrcode () {
       let _qrcodeUrl = `${config.mHost}paydone/billcode-page.html?userid=` + this.userid
-      console.log(_qrcodeUrl)
       let qrcode = document.createElement('canvas')
       QRCode.toCanvas(qrcode, _qrcodeUrl, {scale: 8, margin: 0}, function (err) {
         if (err) throw err
@@ -68,18 +74,13 @@ export default {
     drawQrcodeCanvas (qrcode) {
       let canvas = document.createElement('canvas')
       let ctx = canvas.getContext('2d')
-      canvas.width = qrcode.width
-      canvas.height = qrcode.height + 50
-      ctx.rect(0, 0, qrcode.width, qrcode.height + 50)
+      canvas.width = window.innerWidth / 2.55
+      canvas.height = window.innerWidth / 2.55
       ctx.fillStyle = '#ffffff'
       ctx.fill()
 
-      ctx.fillStyle = '#fe9b20'
-      ctx.font = 'bold 22px 黑体'
-      ctx.textAlign = 'center'
-
       let qrcodeCtx = qrcode.getContext('2d')
-      let imgData = qrcodeCtx.getImageData(0, 0, qrcode.width, qrcode.height)
+      let imgData = qrcodeCtx.getImageData(0, 0, window.innerWidth, window.innerWidth)
       ctx.putImageData(imgData, 0, 0)
       document.getElementById('qrcodeContainer').append(canvas)
     }
@@ -91,23 +92,13 @@ export default {
 @import "../../styles/global.scss";
 .container {
   width: 100%;
-  min-height: 100vh;
   .bg-view {
     width: 652px;
     height: 908px;
-    margin: 200px auto 0;
+    margin: 150px auto 0;
     background: url('./img/bg.png') no-repeat;
     background-size: 100% 100%;
     overflow: hidden;
-  }
-}
-.canvasView {
-  canvas {
-    width: 52%;
-    margin-top: 28%;
-    border: 3px solid #f2932f;
-    border-radius: 6px;
-    padding: 10px 10px 0;
   }
 }
 
