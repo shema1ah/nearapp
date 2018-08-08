90e1029acfafb9064ca573b065dbde9ef62c3f1f
liuyuan <liuyuan@qfpay.com>
2018-08-08 19:04:46 +0800

    到账记录，请求1个月数据且大于21条，停止加载，滑动到底部加载下一个月且大于41条，停止加载，滑动到底部加载最后一个月

--- src/pages/arrival-record/views/balance.vue
+++ src/pages/arrival-record/views/balance.vue
@@ -1,84 +1,81 @@
 <template>
   <div>
-    <div class="loading_box" v-if="hasdata">
-      <div class="balance_container">
-        <div class="balance_box">
-          <p class="balance_text">余额  (元)</p>
-          <p class="balance">{{amt | formatCurrencyStr | formatCurrencyThree}}</p>
+    <div class="balance_container">
+      <div class="balance_box">
+        <p class="balance_text">余额  (元)</p>
+        <p class="balance">{{amt | formatCurrencyStr | formatCurrencyThree}}</p>
+      </div>
+      <img v-if="wx_oauth_mchnt" class="wechat-icon" @click="viewWechatDetail()" src="../assets/wechat-icon.png" alt="微信特约商户">
+    </div>
+    <div class="tab">
+      <p>划款时间</p>
+      <p>划款金额</p>
+      <p>划款状态</p>
+    </div>
+    <ul class="record_list_container">
+      <li class="record_list" v-for="(items, index) in recordList">
+        <!-- 单笔记录 -->
+        <div class="one_record" v-for="item in items" v-if="items.length === 1" @click="godetail(item.biz_sn, item.state)">
+          <p class="date">
+            <span class="day">{{item.ctime | splitDate}}</span>
+            <span class="week">星期{{item.ctime | format | formatWeekDay}}</span>
+          </p>
+          <p class="money"><sub>￥</sub><span>{{item.amt | formatCurrencyStr | formatCurrencyThree}}</span></p>
+          <p class="status">
+            <span :class="{'processes' : item.state === 1 , 'success' : item.state === 2 || item.state === 4, 'fail' : item.state === 3}">{{statusText(item.state)}}</span>
+            <span class="arrow"></span>
+          </p>
+          <div class="fail_tips" v-if="item.state === 3">
+            <span class="triangle"></span>
+            <span>划款失败的款项会退回到您的余额中，银行会为您重新划款。</span>
+          </div>
         </div>
-        <img v-if="wx_oauth_mchnt" class="wechat-icon" @click="viewWechatDetail()" src="../assets/wechat-icon.png" alt="微信特约商户">
-      </div>
-      <div class="tab">
-        <p>划款时间</p>
-        <p>划款金额</p>
-        <p>划款状态</p>
-      </div>
-      <ul class="record_list_container">
-        <li class="record_list" v-for="(items, index) in recordList" v-if="(index != recordList.length - 1) || nomore">
-          <!-- 单笔记录 -->
-          <div class="one_record" v-for="item in items" v-if="items.length === 1" @click="godetail(item.biz_sn, item.state)">
+        <!-- 多笔记录 -->
+        <div class="multiple_record_container" v-if="items.length > 1">
+          <div class="multiple_record">
             <p class="date">
-              <span class="day">{{item.ctime | splitDate}}</span>
-              <span class="week">星期{{item.ctime | format | formatWeekDay}}</span>
+              <span class="day">{{items[0].ctime | splitDate}}</span>
+              <span class="week">星期{{items[0].ctime | format | formatWeekDay}}</span>
             </p>
-            <p class="money"><sub>￥</sub><span>{{item.amt | formatCurrencyStr | formatCurrencyThree}}</span></p>
+            <p class="total"><sub>￥</sub><span>{{currentDayAmtTotal(items) | formatCurrencyStr | formatCurrencyThree}}</span></p>
             <p class="status">
-              <span :class="{'processes' : item.state === 1 , 'success' : item.state === 2 || item.state === 4, 'fail' : item.state === 3}">{{statusText(item.state)}}</span>
-              <span class="arrow"></span>
+              <span class="tips">分<span>{{items.length}}</span>笔到账</span>
             </p>
-            <div class="fail_tips" v-if="item.state === 3">
-              <span class="triangle"></span>
-              <span>划款失败的款项会退回到您的余额中，银行会为您重新划款。</span>
-            </div>
           </div>
-          <!-- 多笔记录 -->
-          <div class="multiple_record_container" v-if="items.length > 1">
-            <div class="multiple_record">
-              <p class="date">
-                <span class="day">{{items[0].ctime | splitDate}}</span>
-                <span class="week">星期{{items[0].ctime | format | formatWeekDay}}</span>
-              </p>
-              <p class="total"><sub>￥</sub><span>{{currentDayAmtTotal(items) | formatCurrencyStr | formatCurrencyThree}}</span></p>
+          <ul>
+            <li class="multiple_record_list" @click="godetail(item.biz_sn, item.state)" v-for="(item, index) in items">
+              <p>第<span>{{index + 1}}</span>笔</p>
+              <p class="money"><sub>￥</sub><span>{{item.amt | formatCurrencyStr | formatCurrencyThree}}</span></p>
               <p class="status">
-                <span class="tips">分<span>{{items.length}}</span>笔到账</span>
+                <span :class="{'processes' : item.state === 1 , 'success' : item.state === 2 || item.state === 4, 'fail' : item.state === 3}">{{statusText(item.state)}}</span>
+                <span class="arrow"></span>
               </p>
-            </div>
-            <ul>
-              <li class="multiple_record_list" @click="godetail(item.biz_sn, item.state)" v-for="(item, index) in items">
-                <p>第<span>{{index + 1}}</span>笔</p>
-                <p class="money"><sub>￥</sub><span>{{item.amt | formatCurrencyStr | formatCurrencyThree}}</span></p>
-                <p class="status">
-                  <span :class="{'processes' : item.state === 1 , 'success' : item.state === 2 || item.state === 4, 'fail' : item.state === 3}">{{statusText(item.state)}}</span>
-                  <span class="arrow"></span>
-                </p>
-                <div class="fail_tips" v-if="item.state === 3">
-                  <span class="triangle"></span>
-                  <span>划款失败的款项会退回到您的余额中，银行会为您重新划款。</span>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </li>
-      </ul>
-      <div class="no_data" v-if="!recordList.length && !firstload">
-        <img src="../../../assets/no-data.png" alt="暂无数据">
-        <p>暂无数据</p>
-      </div>
-    </div>
-    <loading :visible='isloading'></loading>
-    <div class="no_more" v-if="nomore && recordList.length">
-      您可登录商户网站查看更多历史数据
-    </div>
+              <div class="fail_tips" v-if="item.state === 3">
+                <span class="triangle"></span>
+                <span>划款失败的款项会退回到您的余额中，银行会为您重新划款。</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+    <infinite-loading @infinite="requestlist" ref="infiniteLoading" spinner="spiral">
+      <span slot="no-more">您可登录商户网站查看更多历史数据</span>
+      <span slot="no-results">暂无数据</span>
+    </infinite-loading>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   /* global _hmt */
-  import loading from 'components/loading/juhua.vue'
+  import InfiniteLoading from 'vue-infinite-loading'
   import bridge from 'methods/bridge-v2'
   import config from 'methods/config.js'
   import util from 'methods/util'
   export default {
+    components: {
+      InfiniteLoading
+    },
     data () {
       return {
         recordList: [],
@@ -86,18 +83,10 @@
         wx_oauth_mchnt: 0,
         page: 1,
         resArr: [],
-        nomore: 0,
-        isloading: false,
-        hasdata: false,
         monthArr: [],
         shopid: '',
-        shopname: '',
-        Loadmore: null,
-        firstload: true
+        shopname: ''
       }
-    },
-    components: {
-      loading
     },
     beforeRouteEnter (to, from, next) {
       next(vm => {
@@ -109,11 +98,7 @@
       this.shopid = this.$route.query.shopid
       this.shopname = this.$route.query.shopname
       window.localStorage.setItem('shopid', this.$route.query.shopid)
-      this.setNavMenu()
-      this.getMonth()
-      this.requestlist()
-    },
-    computed: {
+      this.calMonths()
     },
     filters: {
       splitDate (item) {
@@ -131,20 +116,6 @@
     },
     mounted () {
       this.appBridge()
-      let _this = this
-      this.Loadmore = function () {
-        let windowScrollTop = window.scrollY
-        let innerHeight = window.innerHeight
-        let scrollHeight = document.body.scrollHeight
-        if (windowScrollTop + innerHeight + 10 >= scrollHeight && !_this.isloading) {
-          if (_this.nomore) {
-            return
-          }
-          _this.page ++
-          _this.requestlist()
-        }
-      }
-      window.addEventListener('scroll', this.Loadmore, false)
     },
     methods: {
       currentDayAmtTotal(items) {
@@ -155,11 +126,6 @@
           }
         })
         return total
-      },
-      appBridge () {
-        bridge.pageRefresh({
-          close: '1'
-        })
       },
       godetail (bizSn, state) {
         switch (state) {
@@ -191,7 +157,7 @@
             return '微信划款'
         }
       },
-      setNavMenu () {
+      appBridge () {
         let urlStr = `${window.location.origin}${window.location.pathname}#/account?shopid=${this.shopid}`
         bridge.setNavMenu({
           buttons: [
@@ -207,25 +173,39 @@
             }
           ]
         })
+        bridge.pageRefresh({
+          close: '1'
+        })
       },
-      getMonth () {
-        let date = new Date()
-        let year = date.getFullYear()
-        let month = date.getMonth() + 1
-        for (let i = 0; i < 3; i++) {
-          if (month === 0) {
-            month = 12
-            year--
-          }
-          month = (month < 10 ? '0' + month : month)
-          let requestMonth = year + '-' + month
-          this.monthArr.push(requestMonth)
-          month--
+      calMonths () {
+        let today = new Date()
+        let year = today.getFullYear()
+        let month = today.getMonth() + 1
+        this.calOneMonth(year, month)
+        if (month === 1) {
+          year -= 1
+          month = 12
+        } else {
+          month -= 1
         }
+        this.calOneMonth(year, month)
+        month = today.getMonth() + 1
+        if (month === 1) {
+          year -= 1
+          month = 11
+        } else if (month === 2) {
+          year -= 1
+          month = 12
+        } else {
+          month -= 2
+        }
+        this.calOneMonth(year, month)
       },
-      requestlist () {
-        this.isloading = true
-        this.firstload = false
+      calOneMonth (year, month) {
+        let monthStr = year + '-' + (month <= 9 ? `0${month}` : month)
+        this.monthArr.push(monthStr)
+      },
+      requestlist($state) {
         this.$http({
           url: `${config.oHost}fund/v1/account/remit/record/`,
           method: 'GET',
@@ -236,48 +216,50 @@
             format: 'cors'
           }
         }).then(response => {
-          this.isloading = false
           let res = response.data
+          let resData = res.data.data
           if (res.respcd === '0000') {
-            this.hasdata = true
-            this.nomore = res.data.data.length || this.monthArr.length !== 1 ? 0 : 1
             this.amt = res.data.amt
             this.wx_oauth_mchnt = res.data.wx_oauth_mchnt
             window.localStorage.setItem('wx_oauth_mchnt', res.data.wx_oauth_mchnt)
-            if (!res.data.data.length && this.monthArr.length > 1) {
+            this.resArr = this.resArr.concat(resData)
+            if (resData.length < 20 && this.monthArr.length > 1) {
               this.monthArr.shift()
               this.page = 1
-              this.requestlist()
-            } else if (this.nomore) {
-              return
-            }
-            this.resArr = this.resArr.concat(res.data.data)
-            if (this.resArr.length < 20 && this.monthArr.length > 1) {
-              this.monthArr.shift()
-              this.page = 1
-              this.requestlist()
-            } else if (this.monthArr.length === 1 && res.data.data.length < 20) {
-              this.page++
-              this.requestlist()
-            }
-            this.recordList.length = 0
-            this.resArr.map((item, index) => {
-              let dataArr = []
-              dataArr.push(item)
-              this.recordList.push(dataArr)
-            })
-            // 数据解析
-            for (let i = 0; i < this.recordList.length - 1; i++) {
-              if (this.recordList[i][this.recordList[i].length - 1].ctime.substring(0, 10) === this.recordList[i + 1][0].ctime.substring(0, 10)) {
-                this.recordList[i] = this.recordList[i].concat(this.recordList[i + 1])
-                this.recordList.splice((i + 1), 1)
-                i--
+              let maxNum = (this.monthArr.length - 2) * 20 + 21
+              if (this.resArr.length > maxNum) {
+                this.renderData()
+                $state.reset()
+              } else {
+                $state.loaded()
               }
+            } else if (resData.length === 20) {
+              this.page++
+              $state.loaded()
+            } else {
+              this.renderData()
+              $state.complete()
             }
           } else {
+            $state.complete()
             this.$toast(res.resperr)
           }
         })
+      },
+      renderData() {
+        this.recordList.length = 0
+        this.resArr.map((item, index) => {
+          let dataArr = []
+          dataArr.push(item)
+          this.recordList.push(dataArr)
+        })
+        for (let i = 0; i < this.recordList.length - 1; i++) {
+          if (this.recordList[i][this.recordList[i].length - 1].ctime.substring(0, 10) === this.recordList[i + 1][0].ctime.substring(0, 10)) {
+            this.recordList[i] = this.recordList[i].concat(this.recordList[i + 1])
+            this.recordList.splice((i + 1), 1)
+            i--
+          }
+        }
       }
     }
   }
